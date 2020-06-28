cb745b9e42156aca8daff9b94ea89db669e759ab
alqasemy2020 <59507561+alqasemy2020@users.noreply.github.com>
2020-06-28 20:43:20 +0300

    Add files via upload

---
+++ sign_up.py
@@ -0,0 +1,175 @@
+name = input("Enter Your Name : ").strip().capitalize().title()
+print()
+name = " ".join(name.split())
+key = ["~","`","!","@","#","$","%","^","&","*","(",")","-","=","[","]","{","}",";",":","\'","\"","|","\\","<",",",".",">","/","?","+"]
+if key[30] in name:
+    print("sorry.You Only Can Write ( \"alpha\" + \"num\" , _ )") 
+else:
+    if key[0] in name:
+        print("sorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+    else:
+        if key[1] in name:
+            print("sorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+        else:
+            if key[2] in name:
+                print("sorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+            else:
+                if key[3] in name:
+                    print("sorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                else:
+                    if key[4] in name:
+                        print("sorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                    else:
+                        if key[5] in name:
+                            print("sorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+
+                        else:
+                            if key[6] in name:
+                                print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+    
+                            else:
+                                if key[7] in name:
+                                    print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+           
+                                else:
+                                    if key[8] in name:
+                                        print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+            
+                                    else:
+                                        if key[9] in name:
+                                            print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                
+                                        else:
+                                            if key[10] in name:
+                                                print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                    
+                                            else:
+                                                if key[11] in name:
+                                                    print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                        
+                                                else:
+                                                    if key[12] in name:
+                                                        print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                            
+                                                    else:
+                                                        if key[13] in name:
+                                                            print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                
+                                                        else:
+                                                            if key[14] in name:
+                                                                print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )")
+                                    
+                                                            else:
+                                                                if key[15] in name:
+                                                                    print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                        
+                                                                else:
+                                                                    if key[16] in name:
+                                                                        print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                            
+                                                                    else:
+                                                                        if key[17] in name:
+                                                                            print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                                
+                                                                        else:
+                                                                            if key[18] in name:
+                                                                                print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                                    
+                                                                            else:
+                                                                                if key[19] in name:
+                                                                                    print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                                        
+                                                                                else:
+                                                                                    if key[20] in name:
+                                                                                        print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                                            
+                                                                                    else:
+                                                                                        if key[21] in name:
+                                                                                            print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                                                
+                                                                                        else:
+                                                                                            if key[22] in name:
+                                                                                                print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                                                    
+                                                                                            else:
+                                                                                                if key[23] in name:
+                                                                                                    print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                                                        
+                                                                                                else:
+                                                                                                    if key[24] in name:
+                                                                                                        print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                                                            
+                                                                                                    else:
+                                                                                                        if key[25] in name:
+                                                                                                            print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                                                                
+                                                                                                        else:
+                                                                                                            if key[26] in name:
+                                                                                                                print("sorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                                                                    
+                                                                                                            else:
+                                                                                                                if key[27] in name:
+                                                                                                                    print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                                                                        
+                                                                                                                else:
+                                                                                                                    if key[28] in name:
+                                                                                                                        print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                                                                            
+                                                                                                                    else:
+                                                                                                                        if key[29] in name:
+                                                                                                                            print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
+                                                                                                                        else:
+                                                                                                                            age = input("Enter your age : ")
+                                                                                                                            b = age.isnumeric()  #----------------------------age-⬇
+                                                                                                                            if b == False or int(age)>=80 :
+                                                                                                                                print("\u001b[31mWrong Age")
+                                                                                                                                print()
+                                                                                                                            else:
+                                                                                                                                print()
+                                                                                                                                gmail = input("Enter your E/Gmail : ")
+                                                                                                                                print()
+                                                                                                                                A =password= input("Enter your Password "" *atleast 7* : ")
+                                                                                                                                c=len(A)
+                                                                                                                                d = c - 5
+                                                                                                                                e = "*" * d
+                                                                                                                                f = A.isnumeric()
+                                                                                                                                j = A.istitle()
+                                                                                                                                if c <7:
+                                                                                                                                    print()   
+                                                                                                                                    print('\u001b[31mat least 7 letters')
+                                                                                                                                else:
+                                                                                                                                    print()
+                                                                                                                                    print(f"Hello, {name}")
+                                                                                                                                    print()
+                                                                                                                                    print(f"You Are {age}")
+                                                                                                                                    print()
+                                                                                                                                    key2="@"           #---------------------------------------email-⬇
+                                                                                                                                    for i in gmail:
+                                                                                                                                        if i ==key2:
+                                                                                                                                            print("Your Gmail Is : "+gmail)
+                                                                                                                                            break
+                                                                                                                                    else:
+                                                                                                                                        print("Your Gmail Is : "+gmail+"@gmail.com")
+                                                                                                                                        print()
+                                                                                                                                        if f == True:
+                                                                                                                                            print(f"\u001b[31mAnd Your Pass** is :\u001b[0m {A.replace(A[3:-2],e)}") # Red
+                                                                                                                                            Change_Pass = input("\u001b[30;1mYour password is very weak \u001b[0m keep or update \u001b[30;1mThe password\u001b[0m:").strip().capitalize()
+                                                                                                                                            if Change_Pass=="Update":
+                                                                                                                                                TheNewPassword = input("Enter the new password \'add some letter\' : ")
+                                                                                                                                                A = TheNewPassword
+                                                                                                                                                cU=len(A)
+                                                                                                                                                dU = cU - 5
+                                                                                                                                                eU = "*" * dU
+                                                                                                                                                fU = A.isnumeric()
+                                                                                                                                                jU = A.istitle()
+                                                                                                                                                
+                                                                                                                                                if jU == False:
+                                                                                                                                                    print(f"\u001b[32mYour New Pass*** is :\u001b[0m {A.replace(A[3:-2],eU)}") # Green 
+                                                                                                                                                else:
+                                                                                                                                                    print(f"\u001b[34mYour New Pass*** is :\u001b[0m {A.replace(A[3:-2],eU)}") # Blue
+                                                                                                                                            else:
+                                                                                                                                                print()
+                                                                                                                                        elif j == False:
+                                                                                                                                            print(f"\u001b[32mAnd Your Pass*** is :\u001b[0m {A.replace(A[3:-2],e)}") # Green
+                                                                                                                                        else:
+                                                                                                                                            print(f"\u001b[34mAnd Your Pass*** is :\u001b[0m {A.replace(A[3:-2],e)}") # Blue
