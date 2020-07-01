4a19b6c35e2f305e2d5738cadbff316010312410
alqasemy2020 <59507561+alqasemy2020@users.noreply.github.com>
2020-07-01 18:22:29 +0300

    Update sign_up.py

--- sign_up.py
+++ sign_up.py
@@ -7,16 +7,15 @@ t = 0
 
 while t < len(key) :  # 31
     if key[t] in name:
-
         print("\u001b[31msorry.You Only Can Write ( \"alpha\" + \"num\" , _ )\u001b[0m")
-        break
     t +=1 
 else:
-    age = input("Enter your age : ")
-    b = age.isnumeric()  #----------------------------age-⬇
-    if b == False or int(age)>=80 :
-        print("\u001b[31mWrong Age\u001b[0m")
+    aT = 0
+    age = input("Enter your age : ") #----------------------------age-⬇
+    if age.isnumeric() == False or int(age)>=90 :
+            print("\u001b[31mWrong Age\u001b[0m")  
     else:
+        print()
         gmail = input("Enter your E/Gmail : ")
         print()
         A =password= input("Enter your Password "" *atleast 7* : ")
@@ -28,15 +27,16 @@ else:
         if c <7:
             print()   
             print('\u001b[31mat least 7 letters\u001b[0m')
-            A =password= input("Enter your Password "" *atleast 7* : ")
+            A =password= input("Enter your Password again *atleast 7* : ")
             print()
         else:
+            print( "-" * 25 )
             print()
             print(f"Hello, {name}")
             print()
             print(f"You Are {age}")
             print()
-            key2="@"           #---------------------------------------email-⬇
+            key2="@"          #---------------------------------------email-⬇
             for i in gmail:
                 if i ==key2:
                     print("Your Gmail Is : "+gmail)
@@ -46,10 +46,12 @@ else:
                 print()
                 if f == True:
                     print(f"\u001b[31mAnd Your Pass** is :\u001b[0m {A.replace(A[3:-2],e)}") # Red
+                    print()
                     Change_Pass = input("\u001b[30;1mYour password is very weak \u001b[0m keep or update \u001b[30;1mThe password\u001b[0m: ").strip().capitalize()
                                                                                                                                            
                     # Updating Option
                     if Change_Pass=="Update":
+                        print()
                         TheNewPassword = input("Enter the new password \'add some letter\' : ")
                         A = TheNewPassword
                         cU=len(A)
@@ -63,10 +65,11 @@ else:
                             print(f"\u001b[34mYour New Pass*** is :\u001b[0m {A.replace(A[3:-2],eU)}") # Blue
                             # Keeping Option
                     elif Change_Pass=="Keep":
-                        print(f"\u001b[30mAnd Your Pass*** is :\u001b[0m {A.replace(A[3:-2],e)}")
+                        print()
+                        print(f"\u001b[31mAnd Your Pass*** is :\u001b[0m {A.replace(A[3:-2],e)}")
                         # Wrong Option
                     else:
-                        print("\u001b[30mworng option choosed\u001b[0m")
+                        print("\u001b[31mworng option choosed\u001b[0m")
 
                 elif j == False:
                     print(f"\u001b[32mAnd Your Pass*** is :\u001b[0m {A.replace(A[3:-2],e)}") # Green
